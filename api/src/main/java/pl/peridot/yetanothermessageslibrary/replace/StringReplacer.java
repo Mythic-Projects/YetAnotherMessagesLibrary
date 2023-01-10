7dfd6d20c77f4446835d4e2ff67ea5c28d1669d9
Peridot <peridot491@protonmail.com>
2023-01-10 12:35:52 +0100

    Add StringReplacer#replace(List, Replaceable[])

--- api/src/main/java/pl/peridot/yetanothermessageslibrary/replace/StringReplacer.java
+++ api/src/main/java/pl/peridot/yetanothermessageslibrary/replace/StringReplacer.java
@@ -1,7 +1,9 @@
 package pl.peridot.yetanothermessageslibrary.replace;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -42,6 +44,15 @@ public final class StringReplacer {
     @Contract(pure = true, value = "null, _, -> null")
     public static String replaceIgnoreCase(@Nullable String text, @NotNull Replaceable... replacements) {
         return replaceIgnoreCase(text, Arrays.asList(replacements));
+    }
+
+    @Contract(pure = true)
+    public static @NotNull List<String> replace(@NotNull List<String> text, @NotNull Replaceable... replacements) {
+        List<String> result = new ArrayList<>();
+        for (String line : text) {
+            result.add(replace(line, replacements));
+        }
+        return result;
     }
 
 }
