274acc0a3226961c1cecaca91db45569452c4317
Peridot <peridot491@protonmail.com>
2023-01-13 12:03:48 +0100

    Create AudienceSupplier#of to easily create simple AudienceSupplier

--- api/src/main/java/pl/peridot/yetanothermessageslibrary/adventure/AudienceSupplier.java
+++ api/src/main/java/pl/peridot/yetanothermessageslibrary/adventure/AudienceSupplier.java
@@ -1,5 +1,6 @@
 package pl.peridot.yetanothermessageslibrary.adventure;
 
+import java.util.function.Function;
 import net.kyori.adventure.audience.Audience;
 import org.jetbrains.annotations.NotNull;
 
@@ -8,5 +9,19 @@ public interface AudienceSupplier<R> {
     @NotNull Audience getAudience(@NotNull R receiver);
 
     boolean isConsole(@NotNull R receiver);
+
+    static @NotNull<R extends Audience> AudienceSupplier<R> of(Function<R, Boolean> checkConsoleFunction) {
+        return new AudienceSupplier<R>() {
+            @Override
+            public @NotNull Audience getAudience(@NotNull R receiver) {
+                return receiver;
+            }
+
+            @Override
+            public boolean isConsole(@NotNull R receiver) {
+                return checkConsoleFunction.apply(receiver);
+            }
+        };
+    }
 
 }
