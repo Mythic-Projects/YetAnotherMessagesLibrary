9ff401478293620c79178d89aac2bac0f0277ea8
Peridot <peridot491@protonmail.com>
2023-03-15 00:40:49 +0100

    Make BukkitLocaleHelper final and make it's constructor private

--- platform/bukkit/src/main/java/dev/peri/yetanothermessageslibrary/util/BukkitLocaleHelper.java
+++ platform/bukkit/src/main/java/dev/peri/yetanothermessageslibrary/util/BukkitLocaleHelper.java
@@ -6,7 +6,7 @@ import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class BukkitLocaleHelper {
+public final class BukkitLocaleHelper {
 
     private static Method GET_LOCALE_SPIGOT;
 
@@ -16,6 +16,9 @@ public class BukkitLocaleHelper {
         } catch (NoSuchMethodException e) {
             GET_LOCALE_SPIGOT = null;
         }
+    }
+
+    private BukkitLocaleHelper() {
     }
 
     public @Nullable static String getLocaleString(@NotNull Player player) {
